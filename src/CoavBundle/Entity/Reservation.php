14e53259b8112aa3e55c3434c4ff2c50e0eebf86
Maxime-Coustes <maxime.coustes@hotmail.fr>
2016-12-14 11:39:26 +0100

    solution quete relations entre entités

---
+++ src/CoavBundle/Entity/Reservation.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reservation
+ */
+class Reservation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $nbSeats;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Reservation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nbSeats
+     *
+     * @param integer $nbSeats
+     * @return Reservation
+     */
+    public function setNbSeats($nbSeats)
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbSeats
+     *
+     * @return integer 
+     */
+    public function getNbSeats()
+    {
+        return $this->nbSeats;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $passengers;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->passengers = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add passengers
+     *
+     * @param \CoavBundle\Entity\User $passengers
+     * @return Reservation
+     */
+    public function addPassenger(\CoavBundle\Entity\User $passengers)
+    {
+        $this->passengers[] = $passengers;
+
+        return $this;
+    }
+
+    /**
+     * Remove passengers
+     *
+     * @param \CoavBundle\Entity\User $passengers
+     */
+    public function removePassenger(\CoavBundle\Entity\User $passengers)
+    {
+        $this->passengers->removeElement($passengers);
+    }
+
+    /**
+     * Get passengers
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPassengers()
+    {
+        return $this->passengers;
+    }
+    /**
+     * @var \CoavBundle\Entity\Flight
+     */
+    private $departure;
+
+
+    /**
+     * Set departure
+     *
+     * @param \CoavBundle\Entity\Flight $departure
+     * @return Reservation
+     */
+    public function setDeparture(\CoavBundle\Entity\Flight $departure = null)
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    /**
+     * Get departure
+     *
+     * @return \CoavBundle\Entity\Flight 
+     */
+    public function getDeparture()
+    {
+        return $this->departure;
+    }
+}
